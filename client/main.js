09775920c5539b6d76bdf14d373523c40c693e8d
Philip Howley <howley.phil@gmail.com>
2014-08-30 12:19:29 -0700

    where we left off

---
+++ client/main.js
@@ -0,0 +1,51 @@
+// counter starts at 0
+//Session.setDefault("counter", 0);
+Session.setDefault("sPlayerEdit", false);
+Session.setDefault("sPlayerId", null);
+
+Template.tMainApp.players = function() {
+  return Players.find();
+};
+
+Template.tMainApp.sPlayerEdit = function() {
+  return Session.get('sPlayerEdit');
+};
+
+// Template.hello.helpers({
+//     counter: function() {
+//         return Session.get("counter");
+//     }
+// });
+
+// Template.hello.events({
+//     'click button': function() {
+//         // increment the counter when button is clicked
+//         Session.set("counter", Session.get("counter") + 1);
+//     }
+// });
+
+Template.tMainApp.events({
+    'click .player-edit': function() {
+        // increment the counter when button is clicked
+        Session.set("sPlayerEdit", true);
+    },
+    'click .player-remove': function() {
+        // increment the counter when button is clicked
+        Session.set("sPlayerEdit", false);
+    },
+    'click .remove-name': function() {
+        // increment the counter when button is clicked
+       
+    },
+    'click .player-add': function(evt, tmpl) {
+        var firstName = tmpl.find('.first-name').value;
+        // increment the counter when button is clicked
+
+        Session.set("sPlayerEdit", false);
+        addPlayer(firstName);
+    }
+});
+
+var addPlayer = function(firstName) {
+  Players.insert({firstName: firstName});
+}
